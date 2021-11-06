5f113b6f6e25556bb30e7ab63ae0fba59384b444
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-11-06 21:24:09 +0300

    container comp

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { BooksProvider } from "../../context/BooksContext";
 import Books from "../Books/Books";
 import Form from "../Form/Form";
+import Header from "../Header/Header";
+
+import styles from "./container.module.css";
 
 function Container() {
   return (
-    <BooksProvider>
-      <Form />
-      <Books />
-    </BooksProvider>
+    <div className={styles.Container}>
+      <Header />
+      <BooksProvider>
+        <Form />
+        <Books />
+      </BooksProvider>
+    </div>
   );
 }
 
