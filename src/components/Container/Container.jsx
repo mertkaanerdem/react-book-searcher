8f86c2a37afbda15922f61309129fcf53abb9a8a
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-11-07 10:48:25 +0300

    searching on single page with navigate react router

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { BooksProvider } from "../../context/BooksContext";
+import Header from "../Header/Header";
+import Form from "../Form/Form";
 import Books from "../Books/Books";
 import Book from "../Book/Book";
 import Footer from "../Footer/Footer";
-import Form from "../Form/Form";
-import Header from "../Header/Header";
+import NotFound from "../NotFound/NotFound";
 
 import styles from "./container.module.css";
 
@@ -19,6 +20,7 @@ function Container() {
         <Routes>
           <Route path="/" element={<Books />} />
           <Route path="/:id" element={<Book />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BooksProvider>
       <Footer />
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,12 +1,15 @@
 import { useBooks } from "../../context/BooksContext";
+import { useNavigate } from "react-router-dom";
 import styles from "./form.module.css";
 function Form() {
   const { searchName, setSearchName, setSearchedName } = useBooks();
 
   //   const onChange = ;
 
+  let navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault(); // enter tıklandığında formun yenilenmesini engeller
+    navigate("/");
     setSearchedName(searchName);
     setSearchName("");
   };
